a28e67c76699bb1ac3a01c2b67d0e3c78623a2b3
iskoric-uniri <skoric.ivona@gmail.com>
2020-02-19 11:18:05 +0100

    added comments

--- randPaths.py
+++ randPaths.py
@@ -17,20 +17,25 @@ class randPaths:
 
     def makeRoute(self):
         # create random path
+        # create graph copy
         newGRAPH = copy.deepcopy(self.GRAPH)
-
+        # initial route
         self.route = []
+        # append start node
         self.route.append(self.startNode)
 
+        # while last node is different from end node
         i = 0
         while self.route[-1] != self.endNode:
             self.availableNodes = []
             available = 0
+            # loop through all nodes
             for j in range(self.numberOfNodes):
                 if newGRAPH[self.route[-1]][j] > 0:
+                    # add which nodes are available
                     self.availableNodes.append(j)
                     available += 1
-
+            # loop through all nodes
             for k in range(self.numberOfNodes):
                 newGRAPH[k][self.route[-1]] = 0
                 newGRAPH[self.route[-1]][k] = 0
@@ -42,6 +47,7 @@ class randPaths:
                 newGRAPH = copy.deepcopy(self.GRAPH)
 
             else:
+                # create random route
                 self.route.append(self.availableNodes[random.randint(0, available - 1)])
                 i += 1
 
