d6b4d0813e31ac4c9928e83aafbe31762fc7ec8c
iskoric-uniri <skoric.ivona@gmail.com>
2020-02-19 11:13:47 +0100

    added comments on random path generator

--- randPaths.py
+++ randPaths.py
@@ -4,43 +4,51 @@
 import random
 import copy
 
+
 class randPaths:
 
-	def __init__(self, GRAPH, NODES, startNode, endNode):
-		self.GRAPH = GRAPH
-		self.numberOfNodes = NODES
-		self.startNode = startNode
-		self.endNode = endNode
-		self.route = []
-		self.availableNodes = []
+    def __init__(self, GRAPH, NODES, startNode, endNode):
+        self.GRAPH = GRAPH
+        self.numberOfNodes = NODES
+        self.startNode = startNode
+        self.endNode = endNode
+        self.route = []
+        self.availableNodes = []
 
-	def makeRoute(self):
-		newGRAPH = copy.deepcopy(self.GRAPH)
+    def makeRoute(self):
+        # create random path
+        # create graph copy
+        newGRAPH = copy.deepcopy(self.GRAPH)
+        # initial route
+        self.route = []
+        # append start node
+        self.route.append(self.startNode)
 
-		self.route = []
-		self.route.append(self.startNode)
+        # while last node is different from end node
+        i = 0
+        while self.route[-1] != self.endNode:
+            self.availableNodes = []
+            available = 0
+            # loop through all nodes
+            for j in range(self.numberOfNodes):
+                if newGRAPH[self.route[-1]][j] > 0:
+                    # add which nodes are available
+                    self.availableNodes.append(j)
+                    available += 1
+            # loop through all nodes
+            for k in range(self.numberOfNodes):
+                newGRAPH[k][self.route[-1]] = 0
+                newGRAPH[self.route[-1]][k] = 0
 
-		i = 0
-		while self.route[-1] != self.endNode:
-			self.availableNodes = []
-			available = 0
-			for j in range(self.numberOfNodes):
-				if newGRAPH[self.route[-1]][j] > 0:
-					self.availableNodes.append(j)
-					available += 1
+            if available == 0:
+                i = 0
+                self.route = []
+                self.route.append(self.startNode)
+                newGRAPH = copy.deepcopy(self.GRAPH)
 
-			for k in range(self.numberOfNodes):
-				newGRAPH[k][self.route[-1]] = 0
-				newGRAPH[self.route[-1]][k] = 0
-			
-			if available == 0:
-				i = 0
-				self.route = []
-				self.route.append(self.startNode)
-				newGRAPH = copy.deepcopy(self.GRAPH)
+            else:
+                # create random route
+                self.route.append(self.availableNodes[random.randint(0, available - 1)])
+                i += 1
 
-			else:
-				self.route.append(self.availableNodes[random.randint(0, available-1)])
-				i += 1
-
-		return self.route
+        return self.route
